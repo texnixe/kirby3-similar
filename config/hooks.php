e1b15d4e99078cf72c39ed5b624e981a64ebc3fc
Sonja Broda <sonja@imap.cc>
2019-04-13 08:36:38 +0200

    Add hooks

--- config/hooks.php
+++ config/hooks.php
@@ -3,16 +3,34 @@
 namespace texnixe\Similar;
 
 return [
+    'page.create:after' => function() {
+        Similar::flush();
+    },
     'page.update:after' => function() {
         Similar::flush();
     },
-    'page.create:after' => function() {
+    'page.delete:after' => function() {
+        Similar::flush();
+    },
+    'page.changeSlug:after' => function() {
+        Similar::flush();
+    },
+    'page.changeStatus:after' => function() {
         Similar::flush();
     },
     'file.create:after' => function() {
         Similar::flush();
     },
     'file.update:after' => function() {
+        Similar::flush();
+    },
+    'file.delete:after' => function() {
+        Similar::flush();
+    },
+    'file.changeName:after' => function() {
+        Similar::flush();
+    },
+    'file.replace:after' => function() {
         Similar::flush();
     }
 ];
